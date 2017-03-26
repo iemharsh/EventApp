282575277028a6db90f6cbf53c918f5383136c4b
iemharsh <harsh.wadhwa.182@gmail.com>
2017-03-26 18:27:39 +0530

    Setup, Initial Components to get Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { EventsAppComponent }  from './event-app.component';
+import {NavBarComponent} from "./nav/navbar.component";
+import {EventsListComponent} from "./events/events-list.component";
+import {EventThumbnailComponent} from "./events/event-thumbnail.component";
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ EventsAppComponent, NavBarComponent, EventsListComponent, EventThumbnailComponent],
+  bootstrap:    [ EventsAppComponent ]
 })
 export class AppModule { }
